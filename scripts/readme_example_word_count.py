3de77951eeaef3e86d408c3e684d150db9c17e7f
Steven Van Ingelgem <steven.vaningelgem@kbc.be>
2021-03-18 16:05:46 +0100

    Using new logo.

--- scripts/readme_example_word_count.py
+++ scripts/readme_example_word_count.py
@@ -2,7 +2,7 @@ from gelanis import Context
 
 counts = (
     Context()
-    .textFile('README.rst')
+    .textFile('README.md')
     .map(lambda line: ''.join(ch if ch.isalnum() else ' ' for ch in line))
     .flatMap(lambda line: line.split(' '))
     .map(lambda word: (word, 1))
